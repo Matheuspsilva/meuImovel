64c938e9a76a63784821427ad9b436f0f21776ee
Matheus <matheuspsilva29@gmail.com>
2021-05-24 22:51:55 -0300

    Adição do index de imóveis

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\RealStateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function(){
+    Route::resources([
+        'real-states' => RealStateController::class,
+
+    ]);
 });
