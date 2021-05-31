46b269bcdf5160882826aa3421783a2624ab4082
Matheus <matheuspsilva29@gmail.com>
2021-05-31 20:29:20 -0300

    Atualizando e recuperado usuário com perfil

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Api\ApiMessages;
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Api\ApiMessages;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -42,10 +43,21 @@ class UserController extends Controller
             return response()->json($message->getMessage(),401);
         }
 
+        Validator::make($data,[
+            'phone' => 'required',
+            'mobile_phone' => 'required'
+        ])->validate();
+
         try {
 
             $data['password'] = bcrypt($data['password']);
             $user = $this->user->create($data);
+            $user->profile()->create(
+                [
+                    'phone' => $data['phone'],
+                    'mobile_phone' => $data['mobile_phone']
+                ]
+            );
 
             return response()->json([
                 'data' => [
@@ -69,7 +81,8 @@ class UserController extends Controller
     {
         try {
 
-            $user = $this->user->findOrFail($id);
+            $user = $this->user->with('profile')->findOrFail($id);
+            $user->profile->social_networks = unserialize($user->profile->social_networks);
 
             return response()->json([
                 'data' => $user
@@ -98,10 +111,20 @@ class UserController extends Controller
             unset($data['password']);
         }
 
+        Validator::make($data,[
+            'profile.phone' => 'required',
+            'profile.mobile_phone' => 'required'
+        ])->validate();
+
         try {
 
+            $profile = $data['profile'];
+            $profile['social_networks'] = serialize($profile['social_networks']);
+
             $user = $this->user->findOrFail($id);
+
             $user->update($data);
+            $user->profile()->update($profile);
 
             return response()->json([
                 'data' => [
---
+++ app/Models/UserProfile.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    protected $table = 'user_profile';
+    protected $fillable = [
+        'phone', 'mobile_phone', 'about', 'social_networks'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
